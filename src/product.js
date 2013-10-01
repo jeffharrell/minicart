7574c99dcdef38bb93633d670d6eacfc3de588ee
Jeff Harrell <jeff@juxtadesign.com>
2013-10-01 00:13:37 -0700

    Adding test cases for item

--- src/product.js
+++ src/product.js
@@ -1,61 +1,56 @@
 'use strict';
 
 
-var Product;
+var eventCache = {};
 
 
-(function () {
+function Product(data) {
+    this._data = data;
+}
 
-    var eventCache = {};
 
+Product.prototype.on = function on(name, event) {
+    var cache = eventCache[name];
 
-    function Product(data) {
-        this._data = data;
+    if (!cache) {
+        cache = eventCache[name] = [];
     }
 
-
-    Product.prototype.on = function on(name, event) {
-        var cache = eventCache[name];
-
-        if (!cache) {
-            cache = eventCache[name] = [];
-        }
-
-        cache.push(event);
-    };
+    cache.push(event);
+};
 
 
-    Product.prototype.fire = function on(name) {
-        var cache = eventCache[name], i, len, fn;
+Product.prototype.fire = function on(name) {
+    var cache = eventCache[name], i, len, fn;
 
-        if (cache) {
-            for (i = 0 , len = cache.length; i < len; i++) {
-                fn = cache[i];
+    if (cache) {
+        for (i = 0 , len = cache.length; i < len; i++) {
+            fn = cache[i];
 
-                if (typeof fn === 'function') {
-                    fn.call(this, Array.prototype.slice.apply(arguments, 1));
-                }
+            if (typeof fn === 'function') {
+                fn.apply(this, Array.prototype.slice.call(arguments, 1));
             }
         }
-    };
+    }
+};
 
 
-    Product.prototype.get = function get(name) {
-        return this._data[name];
-    };
+Product.prototype.get = function get(name) {
+    return this._data[name];
+};
 
 
-    Product.prototype.set = function set(name, value) {
-        var item = this._data[name] = value;
-        this.fire('change', name, value);
-    };
+Product.prototype.set = function set(name, value) {
+    var item = this._data[name] = value;
+    this.fire('change', name, value);
+};
 
 
-})();
+Product.prototype.destroy = function destroy() {
+    this._data = [];
+    this.fire('destroy', this);
+};
 
 
 
-// Export for CommonJS environments
-if (typeof module === 'object' && typeof module.exports === 'object') {
-    module.exports = Product;
-}
+module.exports = Product;
---
+++ test/product.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var assert = require('assert'),
+    Product = require('../src/product');
+
+
+describe('Product Model', function() {
+
+    var item;
+
+
+    beforeEach(function () {
+        item = new Product({
+            name: 'Foo',
+            desc: 'This is an item, foo',
+            amount: 1.23
+        });
+    });
+
+
+    afterEach(function () {
+        item = null;
+    });
+
+
+    it('get() returns valid data', function() {
+        assert.equal(item.get('name'), 'Foo');
+        assert.equal(item.get('desc'), 'This is an item, foo');
+        assert.equal(item.get('amount'), 1.23);
+    });
+
+
+    it('set() writes data', function() {
+        item.set('name', 'Bar');
+        item.set('desc', 'Where is the bar?');
+        item.set('amount', 4.56);
+
+        assert.equal(item.get('name'), 'Bar');
+        assert.equal(item.get('desc'), 'Where is the bar?');
+        assert.equal(item.get('amount'), 4.56);
+    });
+
+
+    it('set() fires an event', function (done) {
+        item.on('change', function (name, value) {
+            assert.equal(name, 'name');
+            assert.equal(value, 'Baz');
+            done();
+        });
+
+        item.set('name', 'Baz');
+    });
+
+
+    it('destroy() empties data', function() {
+        item.destroy();
+
+        assert.equal(item.get('name'), undefined);
+        assert.equal(item.get('desc'), undefined);
+        assert.equal(item.get('amount'), undefined);
+    });
+
+
+    it('destroy() fires an event', function (done) {
+        item.on('destroy', function (item2) {
+            assert.equal(item, item2);
+            done();
+        });
+
+        item.destroy();
+    });
+});
