044339520a1f090516135a43cf91c9745040cfb3
Jeff Harrell <jeff@juxtadesign.com>
2013-10-12 10:22:31 -0700

    UI spacing issues

--- src/config.js
+++ src/config.js
@@ -49,10 +49,10 @@ var config = module.exports = {
         '#PPMiniCart { display: none; }' +
         '#PPMiniCart form { position: absolute; top: 50%; left: 50%; width: 300px; max-height: 400px; margin-left: -150px; margin-top: -200px; padding: 10px; background: #fff url(http://www.minicartjs.com/build/images/minicart_sprite.png) no-repeat -125px -60px; border: 1px solid #999; border-radius: 5px; box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.2); font: 13px/normal arial, helvetica; color: #333; }' +
         '#PPMiniCart ul { margin: 45px 0 13px; padding: 0; list-style-type: none; border-bottom: 1px solid #ccc; }' +
-        '#PPMiniCart .minicart-item { position: relative; height: 30px; }' +
-        '#PPMiniCart .minicart-item a { position: absolute; top: 0; left: 0; width: 185px; text-decoration: none; }' +
+        '#PPMiniCart .minicart-item { position: relative; height: 30px; padding: 6px; border-top: 1px solid #f2f2f2; }' +
+        '#PPMiniCart .minicart-item a { position: absolute; top: 6px; left: 0; width: 185px; text-decoration: none; }' +
         '#PPMiniCart .minicart-quantity { position: absolute; left: 190px; width: 30px; }' +
-        '#PPMiniCart .minicart-remove { position: absolute; top: 5px; left: 230px; width: 14px; height: 14px; background: url(http://www.minicartjs.com/build/images/minicart_sprite.png) no-repeat -134px -4px; border: 0; cursor: pointer; }' +
+        '#PPMiniCart .minicart-remove { position: absolute; top: 10px; left: 230px; width: 14px; height: 14px; background: url(http://www.minicartjs.com/build/images/minicart_sprite.png) no-repeat -134px -4px; border: 0; cursor: pointer; }' +
         '#PPMiniCart .minicart-price { display: block; text-align: right; }' +
         '#PPMiniCart .minicart-subtotal { float: left; font-weight: bold; }' +
         '#PPMiniCart .minicart-submit { float: right; padding: 1px 4px; background: #ffa822 url(http://www.minicartjs.com/build/images/minicart_sprite.png) repeat-x left center; border: 1px solid #d5bd98; border-right-color: #935e0d; border-bottom-color: #935e0d; border-radius: 2px; cursor: pointer; }',
